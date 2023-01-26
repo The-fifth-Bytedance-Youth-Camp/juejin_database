ed099cf213d2fc1be39b65203b75b3ce929e57eb
自然醒 <2507983704@qq.com>
2023-01-26 14:17:55 +0800

    feat(post): initialize the person database

--- juejin_person/person.sql
+++ juejin_person/person.sql
@@ -3,27 +3,30 @@ USE juejin_person;
 
 CREATE TABLE admin
 (
-	id       INT PRIMARY KEY AUTO_INCREMENT,
-	name     VARCHAR(255) NOT NULL,
-	email    VARCHAR(255) NOT NULL,
-	password CHAR(32)     NOT NULL
+	id        INT PRIMARY KEY AUTO_INCREMENT,
+	name      VARCHAR(255) NOT NULL,
+	email     VARCHAR(255) NOT NULL,
+	password  CHAR(32)     NOT NULL,
+	is_delete TINYINT(1)   NOT NULL DEFAULT 0
 );
 
 CREATE TABLE role
 (
-	id       INT PRIMARY KEY,
-	is_super TINYINT(1) NOT NULL,
-	is_front TINYINT(1) NOT NULL,
-	is_audit TINYINT(1) NOT NULL,
+	id        INT PRIMARY KEY,
+	is_super  TINYINT(1) NOT NULL,
+	is_front  TINYINT(1) NOT NULL,
+	is_audit  TINYINT(1) NOT NULL,
+	is_delete TINYINT(1) NOT NULL DEFAULT 0,
 	FOREIGN KEY (id) REFERENCES admin (id)
 );
 
 CREATE TABLE user
 (
-	id       INT PRIMARY KEY AUTO_INCREMENT,
-	name     VARCHAR(255) NOT NULL,
-	email    VARCHAR(255) NOT NULL,
-	password CHAR(32)     NOT NULL
+	id        INT PRIMARY KEY AUTO_INCREMENT,
+	name      VARCHAR(255) NOT NULL,
+	email     VARCHAR(255) NOT NULL,
+	password  CHAR(32)     NOT NULL,
+	is_delete TINYINT(1)   NOT NULL DEFAULT 0
 );
 
 CREATE TABLE permission
@@ -31,5 +34,6 @@ CREATE TABLE permission
 	id        INT PRIMARY KEY,
 	is_allow  TINYINT(1) NOT NULL,
 	is_author TINYINT(1) NOT NULL,
+	is_delete TINYINT(1) NOT NULL DEFAULT 0,
 	FOREIGN KEY (id) REFERENCES user (id)
 );
